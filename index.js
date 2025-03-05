ab77eb83eee1a06c746e3596d0d0f74bbedf4a0a
klxcoder <klxcoder@gmail.com>
2025-03-06 06:16:37 +0700

    draw random particles

--- index.js
+++ index.js
@@ -5,12 +5,43 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 class Particle {
-
+  constructor(effect) {
+    this.effect = effect;
+    this.x = Math.random() * this.effect.width;
+    this.y = Math.random() * this.effect.height;
+    this.radius = 15;
+  }
+  draw(context) {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fillStyle = 'white';
+    context.fill();
+  }
 }
 
 class Effect {
-
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.particles = [];
+    this.numberOfParticles = 20;
+    this.createParticles();
+  }
+  createParticles() {
+    for (let i = 0; i < this.numberOfParticles; i++) {
+      this.particles.push(new Particle(this));
+    }
+  }
+  handleParticles(context) {
+    this.particles.forEach((particle) => {
+      particle.draw(context);
+    });
+  }
 }
+
+const effect = new Effect(canvas);
+effect.handleParticles(ctx);
 
 function animate() {
   //
