15d845b311050b68cec4670af0595c78c4974710
klxcoder <klxcoder@gmail.com>
2025-03-06 07:19:04 +0700

    make particles smaller and slower

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ ctx.strokeStyle = 'white';
 class Particle {
   constructor(effect) {
     this.effect = effect;
-    this.radius = Math.random() * 40 + 5;
+    this.radius = Math.random() * 5 + 2;
     this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2);
     this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2);
-    this.vx = Math.random() * 4 - 2;
-    this.vy = Math.random() * 4 - 2;
+    this.vx = Math.random() * 1 - 0.5;
+    this.vy = Math.random() * 1 - 0.5;
   }
   draw(context) {
     context.beginPath();
